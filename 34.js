3e8f8f064f5ca4e0aae69328a339dd97b9ea08ec
Wendel Soares <wendel.soares.dev@gmail.com>
2022-01-27 12:59:57 -0300

    Não resolvido

---
+++ 34.js
@@ -0,0 +1,4 @@
+/**
+ * Construa uma função que receberá duas Strings de tamanhos variados e que retornará True ou False caso
+todos os caracteres (independentemente de ser maiúsculo ou minúsculo) estejam contidos em ambas palavras.
+ */
