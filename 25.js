42d17d16c6d9e34b682f6fabef288d7b593148f6
Wendel Soares <wendel.soares.dev@gmail.com>
2022-01-26 22:14:17 -0300

    exercício 25

---
+++ 25.js
@@ -0,0 +1,14 @@
+/**
+ * Escrever um programa para exibir os números de 1 até 50 na tela
+ */
+
+const umACinquenta = (n = 0) =>
+{
+    while(n < 50)
+    {
+        n++
+        console.log(`${n}`)
+    }
+} 
+
+umACinquenta()
