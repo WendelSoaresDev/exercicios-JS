ab3042d4cf65118f0d54bda4ad3651ddc1c409a0
Wendel Soares <wendel.soares.dev@gmail.com>
2022-01-26 22:10:22 -0300

    23 em branco, 24 resolvido!

---
+++ 24.js
@@ -0,0 +1,14 @@
+/**
+ * Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+ */
+
+let repeticao = (n) => {
+    while (n < 11)
+    {
+        n++
+        console.log(`${n} - Hello world!`)
+
+    }
+}
+
+repeticao(0)
